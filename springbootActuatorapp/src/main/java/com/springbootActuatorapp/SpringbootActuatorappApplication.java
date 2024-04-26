b6a4d565a66511743caf4e242d659c6e9a149733
Dell <Dell@192.168.1.11>
2024-04-26 15:45:19 +0530

    my second project

---
+++ springbootActuatorapp/src/main/java/com/springbootActuatorapp/SpringbootActuatorappApplication.java
@@ -0,0 +1,13 @@
+package com.springbootActuatorapp;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootActuatorappApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootActuatorappApplication.class, args);
+	}
+
+}
---
+++ springbootActuatorapp/src/test/java/com/springbootActuatorapp/SpringbootActuatorappApplicationTests.java
@@ -0,0 +1,13 @@
+package com.springbootActuatorapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringbootActuatorappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
