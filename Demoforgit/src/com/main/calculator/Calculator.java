642175882d96fea2e2fbf7d7fa5b0719d550e54d
SbillariS <110086298+SbillariS@users.noreply.github.com>
2024-05-30 18:00:20 +0530

    Update Calculator.java
    add +num3 in print statament by hitesh

--- Demoforgit/src/com/main/calculator/Calculator.java
+++ Demoforgit/src/com/main/calculator/Calculator.java
@@ -5,6 +5,6 @@ public class Calculator
     public void addition(int num1, int num2)
     {
     	int num3=num1+num2;
-    	System.out.println("addition of num1 and num2 is ");
+    	System.out.println("addition of num1 and num2 is "+num3);
     }
 }
